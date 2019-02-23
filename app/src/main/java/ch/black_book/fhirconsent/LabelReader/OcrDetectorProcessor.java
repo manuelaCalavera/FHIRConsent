5b7a95c4f369942d2f223b8337da417a8154b468
manny <manuelacalavera@gmail.com>
2019-02-23 23:49:46 +0100

    Label reader now working, thank you Jeremy <3

--- app/src/main/java/ch/black_book/fhirconsent/LabelReader/OcrDetectorProcessor.java
+++ app/src/main/java/ch/black_book/fhirconsent/LabelReader/OcrDetectorProcessor.java
@@ -1,9 +1,13 @@
 package ch.black_book.fhirconsent.LabelReader;
 
-
+import android.graphics.Rect;
 import android.util.Log;
 import android.util.SparseArray;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
@@ -37,12 +41,30 @@ public class OcrDetectorProcessor implements Detector.Processor<TextBlock> {
     @Override
     public void receiveDetections(Detector.Detections<TextBlock> detections) {
         boolean foundName = false;
-        boolean foundCode= false;
+        boolean foundCode = false;
+        boolean foundDob = false;
         boolean tooManyLabels = false;
 
         PatientRecord theRecord = new PatientRecord();
         TextBlock nameBlock = null;
         TextBlock codeBlock = null;
+
+        Rect localRect = new Rect();
+        mGraphicOverlay.getLocalVisibleRect (localRect);
+        mGraphicOverlay.clear();
+
+        /*A bunch of image collection. Use this if scaling the image size
+        int imgHeight = mGraphicOverlay.getHeight();
+        float imgTranslationY = mGraphicOverlay.getTranslationY();
+        int imgTop = mGraphicOverlay.getTop();
+        int imgBot = mGraphicOverlay.getBottom();
+        Rect globRect = new Rect();
+        Point globOffset = new Point();
+        int[] screenLoc = new int[2];
+        int[] windowLoc = new int[2];
+        mGraphicOverlay.getGlobalVisibleRect (globRect,globOffset);
+        mGraphicOverlay.getLocationOnScreen(screenLoc);
+        mGraphicOverlay.getLocationInWindow(windowLoc);*/
 
         /**
          * Checking the TextBlocks to match the USZ small patient label
@@ -55,62 +77,79 @@ public class OcrDetectorProcessor implements Detector.Processor<TextBlock> {
          *| Patient ID                      |
          *|_________________________________|
          * */
-        mGraphicOverlay.clear();
+
         SparseArray<TextBlock> items = detections.getDetectedItems();
         for (int i = 0; i < items.size(); ++i) {
             TextBlock item = items.valueAt(i);
-            Pattern p = Pattern.compile("(\\w+[\\s]*[-\\w]*)[\\r\\n]+\\s*(\\w+[\\s]*[-\\w]*)[\\r\\n]+\\s*((0{0,1}[1-9]|[12][0-9]|3[01])[- \\/.](0{0,1}[1-9]|1[012])[- \\/.](\\d{4}))",0);
-            Matcher m = p.matcher( item.getValue() );
 
-            if (m.lookingAt()) {
-                if (foundName) {
-                    tooManyLabels = true;
+            /*Log.d(logTag, "."
+                    + "\nitem  top: " + item.getCornerPoints()[0].y
+                    + "\nlocal top: " + localRect.top
+                    + "\nitem  bot: " + item.getCornerPoints()[2].y
+                    + "\nlocal bot: " + localRect.bottom);*/
+
+            if (       (item.getCornerPoints()[0].y > localRect.top)        // item is lower than top
+                    && (item.getCornerPoints()[2].y < localRect.bottom)     // item is higher than bottom
+                    && (item.getCornerPoints()[0].x > localRect.left + 50)  // item is right of margin
+                    && (item.getCornerPoints()[1].x < localRect.right - 50))// item is left of margin
+            {
+
+                //match with the name and DOB
+                Pattern p = Pattern.compile("(\\w+[\\s]*[-\\w]*)[\\r\\n]+\\s*(\\w+[\\s]*[-\\w]*)[\\r\\n]+\\s*((0{0,1}[1-9]|[12][0-9]|3[01])[- \\/.](0{0,1}[1-9]|1[012])[- \\/.](\\d{4}))", 0);
+                Matcher m = p.matcher(item.getValue());
+                if (m.lookingAt()) {
+                    if (foundName) {
+                        tooManyLabels = true;
+                    }
+                    OcrGraphic graphic = new OcrGraphic(mGraphicOverlay, item);
+                    mGraphicOverlay.add(graphic);
+
+                    theRecord.LastName = m.group(1);
+                    theRecord.FirstName = m.group(2);
+                    theRecord.DateString = m.group(3);
+                    //Log.d(logTag, "Name matched: " + theRecord.FirstName + " " + theRecord.LastName + " : " + theRecord.DateString);
+                    foundName = true;
+                    nameBlock = item;
+
+                    // check if group 3 matches a Date
+                    Calendar dobCalendar = Calendar.getInstance();
+                    SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+                    Date dob = null;
+                    try {
+                        dob = formatter.parse(m.group(3));
+                        foundDob = true;
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                        foundDob = false;
+                    }
+                    dobCalendar.setTime(dob);
                 }
 
-                OcrGraphic graphic = new OcrGraphic(mGraphicOverlay, item);
+                //match with a code
+                p = Pattern.compile("^(\\w+)$", 0);
+                m = p.matcher(item.getValue());
+                if (m.lookingAt()) {
+                    if (foundCode) {
+                        tooManyLabels = true;
+                    }
+                    OcrGraphic graphic = new OcrGraphic(mGraphicOverlay, item);
+                    mGraphicOverlay.add(graphic);
 
-                mGraphicOverlay.add(graphic);
-
-                theRecord.LastName = m.group(1);
-                theRecord.FirstName = m.group(2);
-                theRecord.DateString = m.group(3);
-
-                Log.d(logTag, "Name matched: "+theRecord.FirstName + " " + theRecord.LastName + " : " + theRecord.DateString);
-
-                foundName = true;
-                nameBlock = item;
-            }
-
-            p = Pattern.compile("^(\\w+)$",0);
-            m = p.matcher( item.getValue() );
-
-            if (m.lookingAt()) {
-                if (foundCode) {
-                    tooManyLabels = true;
+                    theRecord.Code = m.group(1);
+                    //Log.d(logTag, "Code matched: " + theRecord.Code);
+                    foundCode = true;
+                    codeBlock = item;
                 }
-
-                OcrGraphic graphic = new OcrGraphic(mGraphicOverlay, item);
-                mGraphicOverlay.add(graphic);
-
-                theRecord.Code = m.group(1);
-
-                Log.d(logTag, "Code matched: "+theRecord.Code);
-
-                foundCode = true;
-                codeBlock = item;
             }
         }
 
-        if (foundName && foundCode && !tooManyLabels)
-        {
-            if(nameBlock.getCornerPoints()[0].y < codeBlock.getCornerPoints()[0].y) {
-                Log.d(logTag, "patient detected: "+theRecord.FirstName + " " + theRecord.LastName + " " + theRecord.DateString + " : " + theRecord.Code);
+        if (foundName && foundDob && foundCode && !tooManyLabels) {
+            if (nameBlock.getCornerPoints()[0].y < codeBlock.getCornerPoints()[0].y) {
+                Log.d(logTag, "patient detected: " + theRecord.FirstName + " " + theRecord.LastName + " " + theRecord.DateString + " : " + theRecord.Code);
                 listener.onPatientDetected(theRecord);
             }
         }
     }
-
-
 
     /**
      * Frees the resources associated with this detection processor.
--- app/src/main/java/ch/black_book/fhirconsent/LabelReader/PatientRecord.java
+++ app/src/main/java/ch/black_book/fhirconsent/LabelReader/PatientRecord.java
@@ -7,7 +7,7 @@ public class PatientRecord {
     public String LastName = "";
     public String FirstName = "";
     public String DateString = "";
-    public Date DateOfBirth = new Date(0);
+    //public Date DateOfBirth = new Date(0);
     public String Code = "";
 
     public PatientRecord()
@@ -15,7 +15,7 @@ public class PatientRecord {
         LastName ="";
         FirstName = "";
         DateString = "";
-        DateOfBirth.setTime(0);
+        //DateOfBirth.setTime(0);
         Code = "";
     }
 }
